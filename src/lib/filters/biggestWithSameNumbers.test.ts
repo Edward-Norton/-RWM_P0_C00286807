73d19deefcf2bfc89272eb9bae2475ddc8db3463
Edward Norton <C00286807@setu.ie>
2025-09-15 12:07:48 +0100

    Re-added fail

--- src/lib/filters/biggestWithSameNumbers.test.ts
+++ src/lib/filters/biggestWithSameNumbers.test.ts
@@ -4,7 +4,7 @@ import { largestNumbers } from './biggestWithSameDigits';
 describe('BiggestWithSameNumber filter', () => {
   it('biggestWithSameNumber sequence', () => {
     const input = [5, 10, 120, 58, 2017, 231];
-    const expected = [5, 10, 210, 85, 7210, 321];
+    const expected = [5, 10, 210, 85, 7210, 231];
     expect(largestNumbers(input)).toEqual(expected);
   });
 });
