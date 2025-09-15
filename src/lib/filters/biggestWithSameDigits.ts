9e44d58a06f3e42d6291816b0d711f16fcdd4a7f
Edward Norton <C00286807@setu.ie>
2025-09-15 12:31:10 +0100

    Changed helper function name to be less vague

--- src/lib/filters/biggestWithSameDigits.ts
+++ src/lib/filters/biggestWithSameDigits.ts
@@ -1,10 +1,10 @@
-// Pure function: return the largest number of a single value
-export function largestNumber(x: number): number{
+// Pure helper function: return the largest number of a single value
+function BiggestWithSameDigits(x: number): number{
     var num = String(x).split('').sort().reverse().join('');
     return Number(num)
 }
 
 // Map over a sequence of numbers
 export function largestNumbers(xs: number[]): number[] {
-    return xs.map(largestNumber)
+    return xs.map(BiggestWithSameDigits)
 }
